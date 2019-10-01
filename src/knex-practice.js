be0f88fb6261eaef709d4f896251cd4d45349fac
EJ <ejg132@gmail.com>
2019-10-01 09:33:02 -0700

    finished grocery sql

---
+++ src/knex-practice.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+});
+
+function searchByName(searchTerm){
+    knexInstance
+        .select('name')
+        .from('shopping_list')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log(result)
+        });
+}
+
+function paginateProducts(page){
+    const productsPerPage = 6;
+    const offset = productsPerPage * (page - 1);
+
+    knexInstance
+        .select('id', 'name', 'price', 'category')
+        .from('shopping_list')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        });
+}
+
+function getByDate(daysAgo){
+    //(now() - '?? days'::INTERVAL, daysAgo)
+
+    knexInstance
+        .select('id', 'name', 'price', 'category')
+        .from('shopping_list')
+        .where('date_added', '>', knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo))
+        .then(result => {
+            console.log(result)
+        });
+}
+
+function totalByCategory(){
+    knexInstance
+        .select('category')
+        .sum('price as total')
+        .from('shopping_list')
+        .groupBy('category')
+        .then(result => {
+            console.log(result)
+        });
+}
+
+// searchByName('wings');
+// paginateProducts(2);
+// getByDate(25);
+totalByCategory();
---
+++ src/practice.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex( {
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .then(result => {
+    console.log(result);
+  });
+
+function searchByProduceName(searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function paginateProducts(page) {
+  const productsPerPage = 10;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+paginateProducts(2);
+  
+searchByProduceName('holo');
+
+console.log('knex and driver installed correctly');
